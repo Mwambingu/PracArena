f6d9381dde22486927afefc9d496d07381462fff
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-27 17:12:25 +0300

    start

---
+++ JavaScript/mos-js/start.js
@@ -0,0 +1,28 @@
+#!/usr/bin/node
+console.log("Hello World!!");
+// variables
+var firstName;
+let secondName;
+//constant
+const thirdName = "asobi";
+
+firstName = 'robert';
+secondName = 'Kakubo';
+
+console.log(firstName, secondName, thirdName);
+
+//Primitive/Value Types
+// strings
+myName = 'Ondieki';
+// numbers
+myAge = 30;
+// boolean
+isMarried = false;
+isWorking = undefined;
+carType = null;
+
+// Dynaming Typing
+let stringName = 'Harold';
+console.log(typeof(stringName));
+stringName = 34;
+console.log(typeof(stringName));
