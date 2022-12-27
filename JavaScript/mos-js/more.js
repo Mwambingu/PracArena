97ec1945cd174dea0909cedb16d5ad676ec77da1
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2022-12-27 17:35:29 +0300

    objects, functions and arrays

---
+++ JavaScript/mos-js/more.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+// Reference Types
+// Objects in JS
+let person = {
+  firstName: 'Roberto',
+  secondName: 'Notcarlos',
+  age: 44
+};
+
+console.log(person);
+// dot notation
+console.log(person.firstName);
+// bracket notation
+console.log(person['secondName']);
+
+//Arrays
+let selectedColors = ['red', 'blue', 'yellow'];
+console.log(selectedColors);
+console.log(selectedColors.length);
+selectedColors.push("Brown");
+console.log(selectedColors);
+
+//Functions
+// Backticks are template literals
+function youSuck(name) {
+  console.log(`${name} You Suck soooo much!!`);
+}
+
+youSuck("Roberto");
+
+console.log(console);
