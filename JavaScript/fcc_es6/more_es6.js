1a42b4e45987e5fa3845594c92e47c870edc2c14
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-02 20:09:39 +0300

    Rest Operators and Arrow Functions

--- JavaScript/fcc_es6/more_es6.js
+++ JavaScript/fcc_es6/more_es6.js
@@ -10,7 +10,6 @@ for (const income of incomes) {
 console.log(total);
 
 // Spread Operator
-
 let example1 = [1,2,3,4,5,6];
 let example2 = [...example1];
 
@@ -25,3 +24,23 @@ let obEx2 = {
 };
 
 console.log(obEx2);
+
+// Rest Operator
+function add(...nums) {
+  let sumNum = 0;
+  for (let num of nums) {
+    sumNum += num;
+  }
+  return sumNum;
+}
+
+console.log(add(1,3,5,6,0,29,44));
+
+// Arrow Functions
+function add2(...nums) {
+  // let total = nums.reduce(function (x, y) {return x+y;});
+  let total = nums.reduce((x, y) => x + y);
+  console.log(total);
+}
+
+add2(4, 5, 7, 8, 12);
