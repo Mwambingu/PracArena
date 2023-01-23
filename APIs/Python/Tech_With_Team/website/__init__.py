88e496b6d9b674a0964d7868c6d59390e1f0c999
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-01-23 19:05:37 +0300

    secret

--- APIs/Python/Tech_With_Team/website/__init__.py
+++ APIs/Python/Tech_With_Team/website/__init__.py
@@ -2,6 +2,6 @@ from flask import Flask
 
 def create_app():
   app = Flask(__name__)
-  app.config['SECRET_KEY'] = 'g5iN88SaNBpeU1K77R1sQwOjfzOUxEmF'
+  app.config['SECRET_KEY'] = 'dev'
 
   return app
