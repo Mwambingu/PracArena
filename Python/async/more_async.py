25b13cdcc6cf3dc273844dcb524092810ab5717a
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-02-01 19:37:39 +0300

    Arranging

--- Python/async/more_async.py
+++ Python/async/more_async.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 # Working with Generators
+import random import randint
+import time
+import asyncio
 from typing import Generator
 
 def odds(start: int, stop: int) -> Generator[int, None, None]:
   for odd in range(start, stop+1, 2):
     yield odd
+
+def randin():
+  time sleep(3)
+  return randint(1, 10)
 
 def main():
   odd_values = [odd for odd in odds(3, 15)]
