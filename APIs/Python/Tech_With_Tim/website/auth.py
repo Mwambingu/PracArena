7d326b5dc4d185f272684ab878e1dd16bcf91374
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-02-14 15:59:58 +0300

    Allowing GET and POST requests

--- APIs/Python/Tech_With_Tim/website/auth.py
+++ APIs/Python/Tech_With_Tim/website/auth.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 
 auth = Blueprint('auth', __name__)
 
@@ -6,14 +6,16 @@ auth = Blueprint('auth', __name__)
 def error():
   return "<h1>You are lost my friend. Welcome to the void</h1>"
 
-@auth.route("login")
+@auth.route("/login", methods=["GET", "POST"])
 def login():
+  data = request.form
+  print(data)
   return render_template("login.html", boolean=True)
 
 @auth.route("logout")
 def logout():
   return render_template("home.html")
 
-@auth.route("sign-up")
+@auth.route("sign-up", methods=["GET", "POST"])
 def sign_up():
   return render_template("sign-up.html")
