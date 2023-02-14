e4449c886251a4791f8894c755e0151302eff915
Tonny Mwambingu <tonny.mwambingu@gmail.com>
2023-02-14 18:44:41 +0300

    Adding remember logged in users

--- APIs/Python/Tech_With_Tim/website/auth.py
+++ APIs/Python/Tech_With_Tim/website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from . import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint('auth', __name__)
 
@@ -19,6 +20,8 @@ def login():
     if user:
       if check_password_hash(user.password, password):
         flash("Logged in successfully!", category="success")
+        login_user(user, remember=True)
+        return redirect(url_for("views.home"))
       else:
         flash("Incorrect password! Try again.", category="error")
     else:
@@ -54,6 +57,7 @@ def sign_up():
         new_user = User(email=email, first_name=first_name, password=generate_password_hash(password1, method="sha256"))
         db.session.add(new_user)
         db.session.commit()
+        login_user(user, remember=True)
         flash("Account created!", category='success')
         return redirect(url_for('views.home'))
 
